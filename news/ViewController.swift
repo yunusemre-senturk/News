fc69de23ace396c47b816f92f1d4221ccf91f02c
Yunus Emre Şentürk <yunusntrk53@hotmail.com>
2024-03-01 14:21:37 +0300

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by Yunus Emre Şentürk on 1.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
